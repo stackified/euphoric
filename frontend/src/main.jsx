d8797a668716b571fde8c4e901db7ccb5729a009
laveshparyani <laveshparyani01@gmail.com>
2026-02-09 14:46:05 +0530

    fix: switch to HashRouter for GitHub Pages compatibility

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { store } from './store/store.js'
 import './index.css'
@@ -9,9 +9,9 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/euphoric">
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
   </React.StrictMode>,
 )
